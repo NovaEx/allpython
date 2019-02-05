c82c9da0e3b8a9a49de0b107a94e45919e471961
Evgeny Antonov <evgeniy9996@gmail.com>
2019-02-05 13:46:50 +0500

    update

---
+++ time30s.py
@@ -0,0 +1,27 @@
+import time
+from threading import Thread
+from threading import Event
+
+
+def time_now():
+    now = time.localtime()
+    return ('\nTime now: {0}:{1}:{2}'.format(now.tm_hour, now.tm_min, now.tm_sec))
+
+
+class TimerThread(Thread):
+    def __init__(self, event):
+        Thread.__init__(self)
+        self.stopped = event
+
+    def run(self):
+        while not self.stopped.wait(4):
+            print(time_now())
+
+stopFlag = Event()
+thread = TimerThread(stopFlag)
+
+thread.start()
+while True:
+    if input('For to stop write space') == " ":
+        stopFlag.set()
+        break
