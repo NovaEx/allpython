73d0c0c1d7eb85cdef53e2250f47f26b9e68bb05
Evgeny Antonov <evgeniy9996@gmail.com>
2019-02-05 15:45:59 +0500

    update

--- 2xThreadsWritetimes.py
+++ 2xThreadsWritetimes.py
@@ -1,9 +1,8 @@
 import subprocess
 from time import sleep
-process1 = subprocess.Popen(['python3.6', 'time30s.py'], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-process2 = subprocess.Popen(['python3.6', 'time30s.py'], shell=True, stdin=subprocess.PIPE)
-sleep(9)
-out = process1.communicate(input=' \n'.encode())[0]
-process2.communicate(input=' \n'.encode())
-print(out.decode())
+process1 = subprocess.Popen(['python3.6', 'time30s.py']) #, stdin=subprocess.PIPE, stdout=subprocess.PIPE
+process2 = subprocess.Popen(['python3.6', 'time30s.py'])
+sleep(3)
+process1.terminate()
+process2.terminate()
 
