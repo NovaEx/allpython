4d5e45100af602478b78e0ae996e9ed26e0ef203
Evgeny Antonov <evgeniy9996@gmail.com>
2019-02-06 10:33:19 +0500

    update and add comment

--- SystemStats.py
+++ SystemStats.py
@@ -6,9 +6,24 @@ import platform
 # диски - свободно/занято, сетевые интерфейсы, тип и версия ОС
 
 
-print('CPU: {0}  CORES: {1} THREADS: {2} LOAD: {3} FREQ_CUR: {4} MHz FREQ_MAX: {5} MHz FREQ_MIN: {6} MHz'.format(platform.processor(), psutil.cpu_count(logical=False), psutil.cpu_count(), '', int(psutil.cpu_freq()[0]), int(psutil.cpu_freq()[2]), int(psutil.cpu_freq()[1])))
-print('MEMORY ALL: {0} MB IN_USE: {1} MB FREE: {2} MB AVAILABLE: {3} MB'.format(int(psutil.virtual_memory().total/1024/1024), int(psutil.virtual_memory().used/1024/1024), int(psutil.virtual_memory().free/1024/1024), int(psutil.virtual_memory().available/1024/1024)))
+print('CPU: {0}  CORES: {1} THREADS: {2} LOAD: {3} % FREQ_CUR: {4} MHz FREQ_MAX: {5} MHz FREQ_MIN: {6} MHz'.format(
+    platform.processor(),                           # Тип проца
+    psutil.cpu_count(logical=False),                # Количество ядер
+    psutil.cpu_count(),                             # Ядра(логические)
+    psutil.cpu_percent(interval=None),              # Нагрузка в данный момент
+    int(psutil.cpu_freq()[0]),                      # Частота в данный момент
+    int(psutil.cpu_freq()[2]),                      # Максимальная частота
+    int(psutil.cpu_freq()[1])))                     # Минимальная
+print('MEMORY ALL: {0} MB IN_USE: {1} MB FREE: {2} MB AVAILABLE: {3} MB'.format(
+    int(psutil.virtual_memory().total/1024/1024),           # Память вся
+    int(psutil.virtual_memory().used/1024/1024),            # Использованная
+    int(psutil.virtual_memory().free/1024/1024),            # Свободная
+    int(psutil.virtual_memory().available/1024/1024)))      # Всего доступно памяти
 print('DISKS: ')
 for part in psutil.disk_partitions():
-    print('     DEV :{0:<15} MOUNT: {1:<30} fstype: {2:<10} USED: {3:<10}  FREE: {4:<10} MB ALL: {5} MB'.format(part.device, part.mountpoint, part.fstype, str(int(psutil.disk_usage(part.mountpoint).used/1024/1024)) + ' MB', str(int(psutil.disk_usage(part.mountpoint).free/1024/1024)) + ' MB', int(psutil.disk_usage(part.mountpoint).total/1024/1024)))
+    print('     DEV :{0:<15} MOUNT: {1:<30} fstype: {2:<10} USED: {3:<10}  FREE: {4:<10} MB ALL: {5} MB'.format(
+        part.device, part.mountpoint, part.fstype,                              # Устройство, точка монтирования и тип фс
+        str(int(psutil.disk_usage(part.mountpoint).used/1024/1024)) + ' MB',    # Занято на разделе
+        str(int(psutil.disk_usage(part.mountpoint).free/1024/1024)) + ' MB',    # Свободное место на разделе
+        int(psutil.disk_usage(part.mountpoint).total/1024/1024)))               # Всего памяти
 
